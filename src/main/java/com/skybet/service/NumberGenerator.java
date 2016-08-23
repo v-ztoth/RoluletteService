9c44d8575b2bb10c2527430e2b998231e5f78881
Zoltan Toth <v-ztoth@expedia.com>
2016-08-23 10:32:35 +0200

    Implement generate method

--- src/main/java/com/skybet/service/NumberGenerator.java
+++ src/main/java/com/skybet/service/NumberGenerator.java
@@ -2,12 +2,14 @@ package com.skybet.service;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 public class NumberGenerator implements INumberGenerator
 {
     @Override
     public Integer generate()
     {
-        return null;
+        return new Random().nextInt(37);
     }
 }
